2df2c81b79b31e7a1d0f996c1c70d86597522e9e
Rafael Nize <rafael.spitzmann@gmail.com>
2018-05-24 11:46:13 -0300

    fixed IAM policy

--- modules/fargate/main.tf
+++ modules/fargate/main.tf
@@ -119,6 +119,79 @@ resource "aws_ecs_service" "main" {
     ]
 }
 
+resource "aws_iam_role" "autoscale_ecs_pol" {
+  name = "test_role"
+  assume_role_policy = "${file("${path.module}/auto_scale_pol.json")}"
+
+}
+
+resource "aws_appautoscaling_target" "target" {
+    service_namespace  = "ecs"
+    resource_id        = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
+    scalable_dimension = "ecs:service:DesiredCount"
+    role_arn           = "${aws_iam_role.autoscale_ecs_pol.arn}"
+    min_capacity       = 1
+    max_capacity       = 3
+}
+
+resource "aws_appautoscaling_policy" "up" {
+  name                    = "ecs_scale_up"
+  service_namespace       = "ecs"
+  resource_id             = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
+  scalable_dimension      = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment = 1
+    }
+  }
+  depends_on = ["aws_appautoscaling_target.target"]
+}
+
+resource "aws_appautoscaling_policy" "down" {
+  name                    = "ecs_scale_down"
+  service_namespace       = "ecs"
+  resource_id             = "service/${aws_ecs_cluster.main.name}/${aws_ecs_service.main.name}"
+  scalable_dimension      = "ecs:service:DesiredCount"
+
+  step_scaling_policy_configuration {
+    adjustment_type         = "ChangeInCapacity"
+    cooldown                = 60
+    metric_aggregation_type = "Maximum"
+
+    step_adjustment {
+      metric_interval_lower_bound = 0
+      scaling_adjustment = -1
+    }
+  }
+  depends_on = ["aws_appautoscaling_target.target"]
+}
+
+resource "aws_cloudwatch_metric_alarm" "service_cpu_high" {
+    alarm_name          = "cpu_utilization_high"
+    comparison_operator = "GreaterThanOrEqualToThreshold"
+    evaluation_periods  = "1"
+    metric_name         = "CPUUtilization"
+    namespace           = "AWS/ECS"
+    period              = "60"
+    statistic           = "Maximum"
+    threshold           = "50"
+
+  dimensions {
+    ClusterName = "${aws_ecs_cluster.main.name}"
+    ServiceName = "${aws_ecs_service.main.name}"
+  }
+
+  alarm_actions = ["${aws_appautoscaling_policy.up.arn}"]
+  ok_actions    = ["${aws_appautoscaling_policy.down.arn}"]
+}
+
+
 output "ecs_service_endpoint" {
     value = "${aws_alb.main.dns_name}"
 }
